4cbbde0ee1a306b8a50b45bbfcbcd69bb4c37a61
nikocer96 <nikocerone96@gmail.com>
2025-01-30 19:55:01 +0100

    one to one without admin

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -5,9 +5,16 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    # OneToOneField RELAZIONE UNO A UNO
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True) 
     
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -16,6 +23,7 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(5)]) #raiting: valutazione
+    # ForeignKey RELAZIONE UNO A MOLTI
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False) # editable=Flase, SIGNIFICA CHE IL CAMPO NON PUO' ESSERE MODIFICATO
